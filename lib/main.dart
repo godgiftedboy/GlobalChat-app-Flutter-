9b2c28de8c6fe4d311e2f84f49869c7153341974
= <=>
2024-05-26 23:44:56 +0545

    added some git ignore files

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:globalchat/firebase_options.dart';
+import 'package:globalchat/secrets/firebase_options.dart';
 import 'package:globalchat/providers/user_provider.dart';
 import 'package:globalchat/screens/splash_screen.dart';
 import 'package:provider/provider.dart';
